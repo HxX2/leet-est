4b5f6de804e4466fae4118f82dc90e2e3ab57dd7
HxX2 <zaka.hanix@gmail.com>
2023-04-27 23:22:21 +0100

    added nextUI theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,11 @@
 import { NextUIProvider } from '@nextui-org/react'
-import { createTheme, getDocumentTheme } from "@nextui-org/react"
+import { getDocumentTheme } from "@nextui-org/react"
 import { useEffect, useState } from 'react';
+import { lightTheme, darkTheme } from "./conf/theme.ts";
 import Toggle from './component/Toggle.tsx'
 import Cards from './component/Cards.tsx';
 import './css/App.css'
 
-const lightTheme = createTheme({
-	type: 'light',
-	theme: {
-		colors: {},
-	}
-})
-
-const darkTheme = createTheme({
-	type: 'dark',
-	theme: {
-		colors: {},
-	}
-})
 
 
 export default function App() {
---
+++ frontend/src/conf/theme.ts
@@ -0,0 +1,39 @@
+import { createTheme } from '@nextui-org/react';
+
+
+export const lightTheme = createTheme({
+	type: 'light',
+	theme: {
+		colors: {
+
+			primaryLight: '#D5FEEF',
+			primaryLightHover: '#BFFDEC',
+			primaryLightActive: '#AEFBED',
+			primaryLightContrast: '#6CD5D6',
+			primary: '#6CD5D6',
+			primaryBorder: '#94F9F0',
+			primaryBorderHover: '#6CD5D6',
+			primarySolidHover: '#4AA9B3',
+			primarySolidContrast: '$white',
+			primaryShadow: '#94F9F0'
+		},
+	}
+})
+
+export const darkTheme = createTheme({
+	type: 'dark',
+	theme: {
+		colors: {
+			primaryLight: '#D5FEEF',
+			primaryLightHover: '#BFFDEC',
+			primaryLightActive: '#AEFBED',
+			primaryLightContrast: '#6CD5D6',
+			primary: '#6CD5D6',
+			primaryBorder: '#94F9F0',
+			primaryBorderHover: '#6CD5D6',
+			primarySolidHover: '#4AA9B3',
+			primarySolidContrast: '$white',
+			primaryShadow: '#94F9F0'
+		},
+	}
+})
