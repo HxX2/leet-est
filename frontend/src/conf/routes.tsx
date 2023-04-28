be992e7fc519cc7de2b1e3b60b69eec72e92e17c
HxX2 <zaka.hanix@gmail.com>
2023-04-28 07:13:01 +0100

    added login page

--- frontend/src/conf/routes.tsx
+++ frontend/src/conf/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../pages/App.tsx'
 import ErrorPage from '../pages/Error.tsx';
+import Login from '../pages/Login.tsx';
+
 
 
 export const router = createBrowserRouter([
@@ -9,5 +11,8 @@ export const router = createBrowserRouter([
 	  element: <App />,
 	  errorElement: <ErrorPage />
 	},
-
+	{
+		path: "login",
+		element: <Login />,
+	  },
   ]);
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,20 +1,40 @@
-import { Card, Container, Row, Text } from "@nextui-org/react";
+import { Card, Container, Row, Text, Image, Button, Spacer } from "@nextui-org/react";
+import Hello from "../assets/waving_hand_3d_default.png";
+import { FtIcon } from "../icons/FTIcon";
+import Background from "../component/Background";
 
 
 export default function Login() {
+	const theme = window.localStorage.getItem('data-theme');
+	const buttonCss = theme === 'dark' ? { color: "$black", bg: "$white" } : { color: "$white", bg: "$black" };
+
+
 	return (
 		<Container>
-			<Card css={{ $$cardColor: '$colors$primary' }}>
-				<Card.Body>
-					<Row justify="center" align="center">
-						<Text h6 size={15} color="white" css={{ m: 0 }}>
-							NextUI gives you the best developer experience with all the features
-							you need for building beautiful and modern websites and
-							applications.
-						</Text>
-					</Row>
-				</Card.Body>
-			</Card>
+			<Background />
+			<Container fluid display="flex" alignItems="center" justify="center" css={{ height: "100vh" }}>
+				<Card css={{ p: "$6", mw: "400px", mh: "600px" }}>
+					<Card.Body>
+						<Row justify="center" align="center" wrap="wrap">
+							<Image
+								width={200}
+								height={150}
+								src={Hello}
+								alt="hello"
+								css={{ margin: "0 0 0 -2vh" }}
+							/>
+							<Text h1 >Welcome back</Text>
+							<Text h6 >leet us help you estimate the future</Text>
+							<Spacer x={10} y={1} />
+							<Button auto css={buttonCss} icon={<FtIcon />}>
+								<Text css={{ color: "inherit" }} size={13} weight="bold">
+									Continue with Intra
+								</Text>
+							</Button>
+						</Row>
+					</Card.Body>
+				</Card>
+			</Container>
 		</Container>
 	)
 }
